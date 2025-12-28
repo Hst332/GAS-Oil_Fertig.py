4a97e0d9737e16ccda0319e9469283cbdcca267e
Hst332 <hst332@gmail.com>
2025-12-28 16:30:31 +0100

    Update oil_price_forecast.py

--- oil_price_forecast.py
+++ oil_price_forecast.py
@@ -108,7 +108,7 @@ def build_signal(df: pd.DataFrame):
         Signal        : {result['signal']}
         ===================================
         """
-        with open("oil_forecast_output.txt", "w", encoding="utf-8") as f:
+    with open("oil_forecast_output.txt", "w", encoding="utf-8") as f:
                 f.write(text)
 
         write_output_txt(result)
